9eb30cd30c71f2345b996ae611241cf9de6a3a52
sms2sahil <sms2sahil@gmail.com>
2014-02-11 10:42:28 +0000

    Modified main file

--- content_editor.php
+++ content_editor.php
@@ -47,7 +47,7 @@ function generate_combo($dep,$user_dept)
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
  <head>
-  <title> New Document </title>
+  <title> CKDemo List </title>
   <meta name="Generator" content="EditPlus">
   <meta name="Author" content="">
   <meta name="Keywords" content="">
